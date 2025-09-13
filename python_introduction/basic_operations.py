e783a9301e00dd44840a251adbea58571d663a43
root <root@MAY.localdomain>
2025-09-13 16:51:35 +0100

    Basic arithmetic operations of addition, subtraction and multiplication.

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+Sum = number1 + number2
+Sub = number1 - number2
+Multiply = number1 * number2
+print(f"Addition of {number1} and {number2} is {Sum}")
+print(f"Subtraction of {number1} and {number2} is {Sub}")
+print(f"Multiplication of {number1} and {number2} is {Multiply}")
