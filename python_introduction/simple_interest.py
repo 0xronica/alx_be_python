1d349ed382e72063bb803e470d8de3e324af81d1
Veronica <amunegaveronica@gmail.com>
2025-09-13 17:21:14 +0100

    Update simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,5 +1,6 @@
 principal = 1000
 rate = 0.05
 time = 3
-Interest = float(rate * time * principal)
-print(f"The simple interest is: {Interest}")
+Interest = (rate * time * principal)
+interest = float(Interest)
+print(f"The simple interest is: {interest}")
