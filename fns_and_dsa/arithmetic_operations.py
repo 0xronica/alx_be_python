dbaaafe899a566683bc0280fc6a0f132b8e3e95f
root <root@MAY.localdomain>
2025-09-28 21:30:43 +0100

    Arithmetic operations in python

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,13 @@
+def perform_operation(num1: float, num2: float, operation: str):
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 == 0:
+            return "DIVIDE_BY_ZERO"   # recognisable flag for main.py
+        return num1 / num2
+    else:
+        return "INVALID_OPERATION"   # for safety in case of invalid input
