2d4f304ff69a901f06ac1fef9a91aa50a91bba23
root <root@MAY.localdomain>
2025-10-05 21:31:39 +0100

    Bank Account

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,22 @@
+class BankAccount:
+    def __init__(self, initial_balance=0):
+        self.account_balance = initial_balance
+    def deposit(self, amount):
+        if amount > 0:
+            self.account_balance += amount
+        else:
+            print("Sorry, you have to deposit a positive amount. Thank you")
+    def withdraw(self, amount):
+        if amount <= 0:
+            print("Your account balance is not sufficient for this withdrawal")
+            return False
+        if amount <= self.account_balance:
+            self.account_balance -= amount
+            return True
+        else:
+            return False
+    def display_balance(self):
+        print(f"Your current account balance is {self.account_balance: .2f}")
+    
+
+
