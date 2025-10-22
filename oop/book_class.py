6bc53324ed98ab8d0c46c8157347dd5d200573ef
root <root@MAY.localdomain>
2025-10-22 12:05:48 +0100

    Magic metho,. Constructor, Destructor in Book dictionary.

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,5 +1,5 @@
 class Book:
-    def __init__(self, title: str, author: str, year: int)
+    def __init__(self, title: str, author: str, year: int):
         self.title = title
         self.author = author
         self.year = year
@@ -7,7 +7,6 @@ class Book:
         print(f"Deleting {self.title}")
     
     def __str__(self):
-        return f"(title), by (author), published in (year)"
-    
+        return f"{self.title}, by {self.author}, published in {self.year}"  
     def __repr__(self):
-        retun f"Book('{sel.title}', '{self.author}', {self.year}')"
+        return (f"Book({self.title}, {self.author}, {self.year}")
